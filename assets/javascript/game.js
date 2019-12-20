310db887555ab11224514ef685b15411d4ffc314
Amazonian32 <ynitsedekralc@gmail.com>
2019-12-19 22:01:19 -0700

    I remembered the function that Rich sent us to randomize the array but it isn't doing it every time.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,13 @@
 var words = ["Mario", "Pac-Man", "Zelda", "FinalFantasy", "Pokemon", "Sonic", "Galaga", "Tetris"]
 var wins = 0;
-var wordIndex = 0;
+var wordsIndex = 0; 
+    
+words.sort(function(a, b){return 0.5 - Math.random()});
+    console.log(words.sort());
+
 
 document.onkeyup = function(event) {
     console.log(event.key);
     var playerGuess = event.key.toLowerCase();
-    document.querySelector(".incorrect").innerText = playerGuess++;
-    
+
 }
