3af73f779ac6afc9edc9dc2ade7b87c7d8d8be4b
Amazonian32 <ynitsedekralc@gmail.com>
2019-12-19 22:20:08 -0700

    created a var for the remaining guesses, made a variable for the random word; found a string method FINALLY that will out put a letter, but still identify with a keycode.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,46 @@
-var words = ["Mario", "Pac-Man", "Zelda", "FinalFantasy", "Pokemon", "Sonic", "Galaga", "Tetris"]
+var words = ["Mario", "PacMan", "Zelda", "FinalFantasy", "Pokemon", "Sonic", "Galaga", "Tetris"]
 var wins = 0;
 var wordsIndex = 0; 
+var guessesRemaining = 15;
     
 words.sort(function(a, b){return 0.5 - Math.random()});
     console.log(words.sort());
 
+var answer = words[Math.floor(Math.random() * words.length)];
+    console.log(answer);
+
+
 
 document.onkeyup = function(event) {
     console.log(event.key);
-    var playerGuess = event.key.toLowerCase();
+    var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(playerGuess);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
