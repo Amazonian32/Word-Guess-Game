a0a68b464a2fdf2b09afd8e697b85167f3668db0
Amazonian32 <ynitsedekralc@gmail.com>
2019-12-19 23:10:57 -0700

    I added more varibles for what is going to be played at in the game. I am also delcaring and calling a test function to test out a temp word before I call out a full list of words; created a for loop that made the correct guesses equal how many "items" or characters are in my temp word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,47 @@
 var words = ["Mario", "PacMan", "Zelda", "FinalFantasy", "Pokemon", "Sonic", "Galaga", "Tetris"]
 var wins = 0;
-var wordsIndex = 0; 
-var guessesRemaining = 15;
-    
-words.sort(function(a, b){return 0.5 - Math.random()});
-    console.log(words.sort());
+// var wordsIndex = 0;
+
+
+words.sort(function (a, b) { return 0.5 - Math.random() });
+console.log(words.sort());
 
 var answer = words[Math.floor(Math.random() * words.length)];
-    console.log(answer);
+console.log(answer);
+var guessesRemaining;
+var incorrectGuesses;
+var correctGuesses;
+
+
+function testGame() {
+    tempWord = "arcade";
+    correctGuesses = [];
+    console.log(tempWord);
 
 
 
-document.onkeyup = function(event) {
-    console.log(event.key);
+
+
+
+
+    for (let i = 0; i < tempWord.length; i++) {
+        correctGuesses.push("_");
+
+    }
+    console.log(correctGuesses.push());
+
+}
+
+
+
+
+
+
+
+document.onkeyup = function (event) {
     var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
     console.log(playerGuess);
 
+};
+testGame();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
